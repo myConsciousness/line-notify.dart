40c5b2e73a9897ba08f6936edb5ff124b54f3c88
Kato Shinya <kato.shinya.dev@gmail.com>
2021-07-26 22:43:35 +0900

    update

--- example/example.dart
+++ example/example.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:line_notify_d/line_notify.dart';
+import 'package:line_notify_d/line_notify_d.dart';
 
 /// The class that demonstrates the [LineNotify].
 class DemoLineNotify {
